fab53d1cc8ce3868548254294e7bb6af9d471f77
TUTWO <2395650867@qq.com>
2018-07-24 14:29:32 +0800

    Add the function about tab selected by default.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 document.getElementById("tabdiv").addEventListener("click", function (evt) {
-    openTab(evt.target.name);
+    openTab(evt);
 });
 
-function openTab(menu) {
+function openTab(evt) {
     let tabcontent = document.getElementsByClassName("tabcontent");
     for (let i = 0; i < tabcontent.length; i++) {
         if (tabcontent[i].style.display === "block") {
@@ -11,14 +11,13 @@ function openTab(menu) {
             break;
         }
     }
-    document.getElementById(menu).style.display = "block";
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        if (tablinks[i].id === "active") {
+            tablinks[i].removeAttribute("id");
+        }
+    }
+    document.getElementById(evt.target.name).style.display = "block";
+    evt.target.id = "active";
 }
 
-function sendMessages(data) {
-    postMessage({ topic: "request", data }, "*");
-}
-
-sendMessages({ "commandName": "initializeMap", "mapId": "map", "initializeConfigs": { "sourceProjection": "EPSG:4326", "mapProjection": "EPSG:4326", "layerSwitcher": [{ "name": "Base Maps", "type": "radio", "layers": [{ "name": "Hybrid", "relatedLayers": ["Satellite", "RoadMap"], "checked": true }, { "name": "Satellite", "relatedLayers": ["Satellite"] }, { "name": "Grid Pad", "relatedLayers": ["GridPad"] }, { "name": "Roads", "relatedLayers": ["RoadLayer"] }] }], "loadedLayers": ["Satellite", "RoadLayer", "RoadMap", "Plats", "GridPad", "Gps Track", "Gps LineLayer", "Pins", "EditOverlay", "SymbolAndLabel"], "editLayer": "EditOverlay", "editSymbolLabelLayer": "SymbolAndLabel", "selectionExclude": [], "allowedLayers": ["Satellite", "RoadLayer", "GridPad", "Aerial"], "backgroundColor": "#d3d3d3", "allowedControls": ["PanZoomBar", "MousePosition"], "groupingConfig": { "EnableGrouping": false, "MaximumShape": 20, "MaximumTiedown": 2 }, "drawStyle": { "fillColor": "#FFFFFF", "fillOpacity": 0.5, "strokeColor": "#0099FF", "strokeDashstyle": "solid", "strokeOpacity": 0.5, "strokeWidth": 1, "pointRadius": 6, "strokeLinecap": "square" }, "labelPlacementSetting": { "ajustForceDisplayItemOption": { "flip": true, "slide": true }, "ajustForceDisplayItemToPervertOverlap": true, "identicalShapeLengthProximityThresholdInFeet": 3, "identicalShapeLengthProximityThresholdInPixel": 7, "identicalShapeLengthProximityType": "Pixel", "identifyLabelAdjustPolylineLabel": true, "identifyLabelIgnoreValue": true, "shrinkXPixelForLabelToCaculateOverLap": 0 } } } )
-setTimeout(() => {
-    sendMessages({ commandName: 'setUserControlOffsets', offsetWidth: 0, offsetHeight: 0 } );
-}, 1000);
