7cd2d11974e3b7c75cbe154643b5460deeec32b8
TUTWO <tu2395650867@outlook.com>
2018-08-10 17:53:26 +0800

    Login.

--- Node/index.js
+++ Node/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+// var passport = require('passport');
+// var passportJWT = require('passport-jwt');
+
+// var ExtractJwt = passportJWT.ExtractJwt;
+// var JwtStrategy = passportJWT.Strategy;
+
+const secret = 'jwt';
+var token;
 
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -19,13 +29,25 @@ app.post('/authenticate', urlencodedParser, function (req, res) {
         var user = data[req.body.name];
         if (user) {
             if (user.password == req.body.password) {
-                res.send(user);
+                var payload = {
+                    'name': user
+                }
+                token = jwt.sign(payload, secret, { expiresIn: '1day' })
+                res.send({ 'user': user, 'secret': 'jwt' });
             }
+        } else {
+            return res.status(401).send('invaild password');
         }
     })
 })
 
-app.get('/map', function (req, res) {
+app.post('/map', urlencodedParser, function (req, res) {
+    // jwt.verify(token, secret, (err, decoded) => {
+    //     if (err) {
+    //         res.send(err);
+    //     }
+    //     res.send(decoded);
+    // })
     res.sendFile(__dirname + '/dist/index.html');
 })
 
